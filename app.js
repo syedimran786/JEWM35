8577880f9584396e06a0df4fd9fb3de61321240f
syedimran786 <syedimranhgmrd@gmail.com>
2021-10-26 10:11:18 +0530

    Js Variables and data types

---
+++ app.js
@@ -0,0 +1,49 @@
+//! Data types in Javascript
+
+/**
+ * ? 1.Number  ---->+2,-2,2.89,22222222222,23.79877766
+ * ? 2.String---->"Jspiders",'Jspiders',`Jspiders`
+ * ? 3.Boolean---> true(1),false(0)
+ * ? 4.Null---> Empty,0
+ * ? 5.Undefined--> var a;  variable is declared value is not assigned
+ */
+
+//! Keywords to declare variable in js
+
+/**
+ * ? 1. var
+ * ? 2. let
+ * ? 3. const
+ */
+
+// var a=20;
+// console.log(a);
+
+// let b=30;
+// console.log(b);
+
+// const c=40;
+// console.log(c);
+
+
+// let a=20;
+// let b="20";
+// console.log(a*2);
+// console.log(a**5);
+
+
+let a=20;
+let b="20";
+let c=20;
+console.log(a);
+console.log(b);
+
+
+console.log(typeof a);
+console.log(typeof b);
+
+console.log(a==b);
+console.log(a===b);
+console.log(a===c);
+
+
